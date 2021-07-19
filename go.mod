d86b63ebe1938c3d8d2e544ad8d09fd9e24e8f97
david kuchersky <davidkuchersky@gmail.com>
2021-07-19 18:03:14 +0300

    after rescaling

--- go.mod
+++ go.mod
@@ -1,12 +1,9 @@
 module photo_blog
 
 go 1.16
-<<<<<<< HEAD
 
 require (
 	github.com/go-redis/redis/v8 v8.11.0
 	github.com/lib/pq v1.10.2
-	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/satori/go.uuid v1.2.0
 )
-=======
->>>>>>> fcd9dd1ea813e41050a0084bc38b039bd560dfcd
--- redis.go
+++ redis.go
@@ -31,36 +31,3 @@ func redisGetSession(uuid string) string {
 	return name
 
 }
-package main
-
-import (
-	"context"
-	"fmt"
-
-	"github.com/go-redis/redis/v8"
-)
-
-// docker run -p 6379:6379 redis
-
-var ctx = context.Background()
-var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
-
-func redisSetSession(name string, uuid string) {
-	err := rdb.Set(ctx, uuid, name, 0).Err()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func redisGetSession(uuid string) string {
-	name, err := rdb.Get(ctx, uuid).Result()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return name
-
-}
