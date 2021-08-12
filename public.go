23c91da78f9eec6344459e3690b2bc953c4e6ba4
david kuchersky <davidkuchersky@gmail.com>
2021-08-12 18:54:41 +0300

    publishing achieved

--- public.go
+++ public.go
@@ -6,6 +6,12 @@ import (
 	"strings"
 )
 
+type Published_gallery struct {
+	Owner        string
+	Gallery_name string
+	Pics         map[string]string
+}
+
 //display a list of public galleries with creators name as links
 func public_galleries(res http.ResponseWriter, req *http.Request) {
 	data := get_published_galleries()
@@ -16,13 +22,13 @@ func public_galleries(res http.ResponseWriter, req *http.Request) {
 
 //displays a published gallery- protected mode
 func display_published(res http.ResponseWriter, req *http.Request) {
-	//must have:
 	creds := req.FormValue("creds")
 	//temp, unifnished
-	split := strings.FieldsFunc()
-	username := req.FormValue("username")
-	gallery_name := req.FormValue("gallery_name")
+	split := strings.Split(creds, "!")
+	username := split[1]
+	gallery_name := split[0]
 	pics := get_pics_annotations(username, gallery_name)
-	tpl.ExecuteTemplate(res, "public_gallery.html", pics)
+	data := Published_gallery{username, gallery_name, pics}
+	tpl.ExecuteTemplate(res, "public_gallery.html", data)
 
 }
